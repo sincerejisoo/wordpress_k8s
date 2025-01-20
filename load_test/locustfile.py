0eea26defaf14cc3f602186017b743e1deac3dbb
Ubuntu <ubuntu@ip-10-0-0-29.ap-northeast-2.compute.internal>
2025-01-20 06:38:35 +0000

    redis installed

---
+++ load_test/locustfile.py
@@ -0,0 +1,17 @@
+from locust import HttpUser, task, between, TaskSet
+
+class UserBehavior(TaskSet):
+    @task
+    def get_user_detail(self):
+        user_id = 1
+        self.client.get(f'/user/{user_id}')
+
+    @task
+    def home(self):
+        self.client.get('/')
+
+
+class LocustUser(HttpUser):
+    host = "<http://localhost:8000>"
+    tasks = [UserBehavior]
+    wait_time = between(1, 4)
